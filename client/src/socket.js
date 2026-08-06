da0018691651ad446da615bf46989029e4a1a2ee
tuyetoanh6807-sketch <tuyetoanh6807@gmail.com>
2026-08-06 19:01:44 +0700

    Delete client directory

--- client/src/socket.js
+++
@@ -1,8 +0,0 @@
-import { io } from 'socket.io-client';
-
-const SOCKET_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:4000';
-
-export const socket = io(SOCKET_URL, {
-  autoConnect: true,
-  transports: ['websocket', 'polling'],
-});
